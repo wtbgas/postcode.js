286a746f6317814ec7ac5acc03e6b97c528e9974
Wayne Taylor <wtbgas@gmail.com>
2017-11-30 15:12:38 +0000

    Create index.d.ts
    Would be nicer as module export but couldn't get anything working in typings for it as it is, other than like this.

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+/** typings for PostCode
+  * usage: 
+  * import PostCode  = require('postcode');
+  * var postCode = new PostCode('A12 B23');
+  */
+
+declare function PostCode(postCode: string): void;
+export = PostCode;
